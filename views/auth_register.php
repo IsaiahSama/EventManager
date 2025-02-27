37f3876055f1e892ae4cb5095adb447bcd54216a
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-27 16:39:25 -0400

    Added content to registration page

--- views/auth_register.php
+++ views/auth_register.php
@@ -1,1 +1,20 @@
-<h1> Welcome to the registration page </h1>
+<h1>Registration Page</h1>
+
+<form action="/register" action="POST">
+	<div>
+		<label for="email">Email</label>
+		<input id="email" type="email" name="email" required>
+	</div>
+	<div>
+		<label for="passsword">Passsword</label>
+		<input id="passsword" type="passsword" name="passsword" required>
+	</div>
+
+	<div>
+		<button type="submit">Register</button>
+	</div>
+</form>
+
+<p>
+	Already have an account? Log in <span><a href="/login">here</a></span>
+</p>
