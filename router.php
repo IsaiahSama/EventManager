7fd98c290ff0c669d442a920537946e4bf6f086c
IsaiahSama <zeldaplayergl@gmail.com>
2025-03-02 13:42:05 -0400

    Router now uses regex and capturing groups to route

--- router.php
+++ router.php
@@ -8,32 +8,32 @@ include_once "controllers/EventController.php";
 $routes = array();
 
 $routes["GET"] = [
-	['/register', "AuthController::getRegisterPage"],
-	['/login', "AuthController::getLoginPage"],
-	['/user', "AuthController::getUser"],
-	['/user/[0-9]+/events', "AuthController::getUserEvents"],
-	['/events', "EventController::getEvents"],
-	['/events/[0-9]+', "EventController::getEvent"],
-	['/events/create', "EventController::createEventPage"],
-	['/events/edit\/[0-9]+', "EventController::editEventPage"],
-	['/events/delete\/[0-9]+', "EventController::deleteEventPage"],
+	['#^/register$#', "AuthController::getRegisterPage"],
+	['#^/login$#', "AuthController::getLoginPage"],
+	['#^/user$#', "AuthController::getUser"],
+	['#^/user/([0-9]+)/events$#', "AuthController::getUserEvents"],
+	['#^/events$#', "EventController::getEvents"],
+	['#^/events/([0-9]+)$#', "EventController::getEvent"],
+	['#^/events/create$#', "EventController::createEventPage"],
+	['#^/events/edit/([0-9]+)$#', "EventController::editEventPage"],
+	['#^/events/delete/([0-9]+)$#', "EventController::deleteEventPage"],
 ];
 
 $routes["POST"] = [
-	['/register', "AuthController::postRegister"],
-	['/api/register', "AuthController::postRegisterAPI"],
-	['/login', "AuthController::postLogin"],
-	['/api/login', "AuthController::postLoginAPI"],
-	['/events', "EventController::createEvent"],
-	['/events/[0-9]+/register', "EventController::registerUser"],
+	['#^/register$#', "AuthController::postRegister"],
+	['#^/api/register$#', "AuthController::postRegisterAPI"],
+	['#^/login$#', "AuthController::postLogin"],
+	['#^/api/login$#', "AuthController::postLoginAPI"],
+	['#^/events$#', "EventController::createEvent"],
+	['#^/events/([0-9]+)/register$#', "EventController::registerUser"],
 ];
 
 $routes["PUT"] = [
-	['/events/[0-9]+', "EventController::updateEvent"],
+	['#^/events/([0-9]+)$#', "EventController::updateEvent"],
 ];
 
 $routes["DELETE"] = [
-	['/events/[0-9]+', "EventController::deleteEvent"],
+	['#^/events/([0-9]+)$#', "EventController::deleteEvent"],
 ];
 
 function route($uri, $method)
@@ -41,10 +41,10 @@ function route($uri, $method)
 	global $routes;
 	$paths = $routes[$method];
 
+	$matches = [];
 	foreach ($paths as $path) {
-		if ($path[0] == $uri) {
-			// if (preg_match($path[0], $uri)) {
-			call_user_func_array($path[1], array());
+		if (preg_match($path[0], $uri, $matches)) {
+			call_user_func_array($path[1], $matches);
 			return;
 		}
 	}
