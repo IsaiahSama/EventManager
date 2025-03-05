33fc61e81221073fbc74cd8ebeef2f3164a63812
IsaiahSama <zeldaplayergl@gmail.com>
2025-03-05 19:22:02 -0400

    Stubbed methods for the frontcontroller

--- event_manager_client/FrontController.php
+++ event_manager_client/FrontController.php
@@ -4,7 +4,20 @@ class FrontController
 {
 
 	// Get Methods
-
+	public static function getRegisterPage($data = []): void {}
+	public static function getLoginPage($data = []): void {}
+	public static function getEventCreatePage($data = []): void {}
+	public static function getEventUpdatePage($data = []): void {}
+	public static function viewEventPage($data = []): void {}
+	public static function viewEventsPage($data = []): void {}
+	public static function getUserEventsPage($data = []): void {}
+	public static function getUserEventRegisterPage($data = []): void {}
 
 	// Post Methods
+
+	public static function postRegister(): void {}
+	public static function postLogin(): void {}
+	public static function postEventCreate(): void {}
+	public static function postEventUpdate(): void {}
+	public static function postUserEventRegister(): void {}
 }
