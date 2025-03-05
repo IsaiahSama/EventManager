5310e0d041291d10b5e25f5962353223cf7e90f3
IsaiahSama <zeldaplayergl@gmail.com>
2025-03-05 14:16:30 -0400

    Added structre file for view pages

--- event_manager_client/view.php
+++ event_manager_client/view.php
@@ -1,7 +1,10 @@
 <?php
 
-function render($path, $data =[]){
+function render($path, $data = [])
+{
 	extract($data);
 
+	require_once "./views/header.php";
 	require_once $path;
+	require_once "./views/footer.php";
 }
---
+++ event_manager_client/views/header.php
@@ -0,0 +1,11 @@
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Event Management System</title>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+</head>
+
+<body>
