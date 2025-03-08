04320e809097928a83fe10a07eb7a249f42bedeb
IsaiahSama <zeldaplayergl@gmail.com>
2025-03-08 06:38:42 -0400

    Can now make post requests

--- event_manager_client/Curler.php
+++ event_manager_client/Curler.php
@@ -22,11 +22,30 @@ class Curler
 		]);
 
 		$result = curl_exec($ch);
+		curl_close($ch);
 		return json_decode($result, true);
 	}
 
 	/**
 	 * @param array<int,mixed> $data
 	 */
-	public static function post(string $url, array $data): void {}
+	public static function post(string $url, array $data): array
+	{
+
+		$targetURL = static::$baseURL . ltrim($url, "/");
+
+		$ch = curl_init();
+		curl_setopt_array($ch, [
+			CURLOPT_URL => $targetURL,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_POST => true,
+			CURLOPT_HTTPHEADER => ['content-type: application/json'],
+			CURLOPT_POSTFIELDS => $data
+		]);
+
+		$result = curl_exec($ch);
+
+		curl_close($ch);
+		return json_decode($result, true);
+	}
 }
