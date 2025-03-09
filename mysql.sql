be6e46bc31eeece5e430827d6f5a041ff2a8d702
Isaiah <63256944+IsaiahSama@users.noreply.github.com>
2025-03-09 11:33:50 -0400

    Updateed mysql.sql file

--- mysql.sql
+++ mysql.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 9.2.0, for Linux (x86_64)
 --
 -- Host: localhost    Database: scalable_assignment
 -- ------------------------------------------------------
--- Server version	10.4.32-MariaDB
+-- Server version	9.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,30 +21,54 @@
 
 DROP TABLE IF EXISTS `event`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `event` (
-  `eventID` int(11) NOT NULL AUTO_INCREMENT,
-  `eventName` varchar(100) DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL,
+  `eventID` int NOT NULL AUTO_INCREMENT,
+  `eventName` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `startDate` date DEFAULT NULL,
   `endDate` date DEFAULT NULL,
   `price` decimal(7,2) DEFAULT NULL,
-  `hostEmail` varchar(40) DEFAULT NULL,
+  `hostEmail` varchar(40) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`eventID`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+INSERT INTO `event` VALUES (1,'Getting','Queens','2025-03-08','2025-03-09',1.00,'test@sample.com'),(2,'SleepyTime2','Bridgetown','2025-05-01','2025-05-04',300.00,'jerry@smith.com'),(3,'ThirdOne','ChristChurch','2024-10-20','2024-10-22',47.00,'jerry@smith.com'),(4,'ThirdOne','ChCh','2024-10-20','2024-10-22',47.00,'jerry@smith.com'),(5,'ThirdOne','ChCh','2024-10-20','2024-10-22',47.00,'jerry@smith.com'),(6,'ThirdOne','ChCh','2024-10-20','2024-10-22',47.00,'jerry@smith.com'),(7,'ThirdOne','ChCh','2024-10-20','2024-10-22',47.00,'jerry@smith.com');
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(40) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `apiKey` varchar(12) DEFAULT NULL,
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(40) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `apiKey` varchar(12) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'test@sample.com','$2y$10$PCy3545R0MDVmSsjqY7RCee6yfXZJb1drH17F2ZD0uiz7KoQp2ija','7pc-evRu-ZWO'),(2,'jerry@smith.com','$2y$10$opwVQJid63QoqbEWsf/hpeYTebNy66gH4gq1neVRHz9PSxVi37fAO','zND-rr9u-aB3');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_event`
@@ -52,15 +76,24 @@ CREATE TABLE `user` (
 
 DROP TABLE IF EXISTS `user_event`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_event` (
-  `userID` int(11) DEFAULT NULL,
-  `eventID` int(11) DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int DEFAULT NULL,
+  `eventID` int DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `user_event`
+--
+
+LOCK TABLES `user_event` WRITE;
+/*!40000 ALTER TABLE `user_event` DISABLE KEYS */;
+INSERT INTO `user_event` VALUES (2,1,1);
+/*!40000 ALTER TABLE `user_event` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -71,4 +104,4 @@ CREATE TABLE `user_event` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-02 20:07:07
+-- Dump completed on 2025-03-09 15:24:11
